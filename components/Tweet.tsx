b826df287c7581d4b391091a130518c7c2811dda
tobexcode <tobexalox@gmail.com>
2022-07-08 09:47:01 +0100

    first commit

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,76 +1,87 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Comment, Tweet, CommentBody } from '../typings';
 import TimeAgo from 'react-timeago';
-import { fetchComments } from '../utils/fetchComments';
 import {
-    ChatAlt2Icon,
-    HeartIcon,
-    SwitchHorizontalIcon,
-    UploadIcon,
-  } from '@heroicons/react/outline';
-  import { useSession } from 'next-auth/react';
-
+  ChatAlt2Icon,
+  HeartIcon,
+  SwitchHorizontalIcon,
+  UploadIcon,
+} from '@heroicons/react/outline';
+import { fetchComments } from '../utils/fetchComments';
+import { useSession } from 'next-auth/react';
+import toast from 'react-hot-toast';
 interface Props {
-    tweet: Tweet
+  tweet: Tweet;
 }
-
 function Tweet({ tweet }: Props) {
-    console.log(tweet)
+  const { data: session } = useSession();
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [commentBoxVisible, setCommentBoxVisible] = useState<boolean>(false);
+  const [input, setInput] = useState<string>('');
+  const refreshComments = async () => {
+    const comments: Comment[] = await fetchComments(tweet._id);
+    setComments(comments);
+  };
+  useEffect(() => {
+    refreshComments();
+  }, []);
 
-    const { data: session } = useSession();
-    console.log(session)
-    const [comments, setComments] = useState<Comment[]>([]);
-    const [commentBoxVisible, setCommentBoxVisible] = useState<boolean>(false);
-    const [input, setInput] = useState<string>(''); 
-
-    const refreshComments = async () => {
-        const comments: Comment[] = await fetchComments(tweet._id);
-        setComments(comments);
+  const postComment = async () => {
+    const commentInfo: CommentBody = {
+      comment: input,
+      tweetId: tweet._id,
+      username: session?.user?.name || 'Unknown User',
+      profileImg:
+        session?.user?.image ||
+        'https://media.istockphoto.com/vectors/user-avatar-profile-icon-black-vector-illustration-vector-id1209654046?k=20&m=1209654046&s=612x612&w=0&h=Atw7VdjWG8KgyST8AXXJdmBkzn0lvgqyWod9vTb2XoE=',
     };
+    const result = await fetch(`/api/addComment`, {
+      body: JSON.stringify(commentInfo),
+      method: 'POST',
+    });
+    const json = await result.json();
+    const newComments = await fetchComments(tweet._id);
+    setComments(newComments);
 
-    useEffect(() => {
-        refreshComments();
-    }, []);
+    toast('Comment Posted.');
+    return json;
+  };
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    postComment();
+    setInput('');
+  };
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault()
-      // postTweet();
-      // setInput('');
-      // setImage('');
-      // setImageUrlBoxIsOpen(false);
-    };
-
-    console.log(comments)
-    console.log(comments)
-
-    return (
-        <div className="flex flex-col space-x-3 border-y border-gray-100 p-5">
-            <div className='flex space-x-3'>
-                <img className="h-10 w-10 rounded-full object-cover
-          "src={tweet.profileImg} alt="profilimg" />
-
-                <div className="">
-                    <div className="flex items-center space-x-1">
-                        <p className="mr-1 font-bold">{tweet.username}</p>
-                        <p className="text-grey-500 hidden text-sm sm:inline">
-                            @{tweet.username.replace(/\s=/g, '').toLowerCase()}
-                        </p>
-                        <TimeAgo
-                            className="text-sm text-gray-500"
-                            date={tweet._createdAt}
-                        />
-                    </div>
-                    <p>{tweet.text}</p>
+  return (
+    <div className="flex flex-col space-x-3 border-y border-gray-100 p-5">
+      <div className="flex space-x-3">
+        <img
+          className="h-10 w-10 rounded-full object-cover
+          "
+          src={tweet.profileImg}
+          alt="profilimg"
+        />
+        <div>
+          <div className="flex items-center space-x-1">
+            <p className="mr-1 font-bold">{tweet.username}</p>
+            <p className="text-grey-500 hidden text-sm sm:inline">
+              @{tweet.username.replace(/\s=/g, '').toLowerCase()}
+            </p>
+            <TimeAgo
+              className="text-sm text-gray-500"
+              date={tweet._createdAt}
+            />
+          </div>
+          <p>{tweet.text}</p>
           {tweet.image && (
             <img
               src={tweet.image}
               className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm"
             />
-          )} 
-                </div>
-            </div>
-
-            <div className="mt-5 flex justify-between">
+          )}
+        </div>
+      </div>
+      <div className="mt-5 flex justify-between">
         <div
           className="flex cursor-pointer items-center space-x-3 text-gray-400"
           onClick={() => session && setCommentBoxVisible(!commentBoxVisible)}
@@ -88,10 +99,8 @@ function Tweet({ tweet }: Props) {
           <UploadIcon className="h-5 w-5" />
         </div>
       </div>
- 
-
       {commentBoxVisible && (
-        <form className="mt-3 flex space-x-3" onSubmit={handleSubmit}>
+        <form className="mt-3 flex space-x-3">
           <input
             value={input}
             onChange={(e) => setInput(e.target.value)}
@@ -101,14 +110,13 @@ function Tweet({ tweet }: Props) {
           />
           <button
             disabled={!input}
-            type='submit'
+            onClick={handleSubmit}
             className="disabled text-twitter disabled:text-gray-100"
           >
             Post
           </button>
         </form>
       )}
-
       {comments?.length > 0 && (
         <div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5">
           {comments.map((comment) => (
@@ -136,8 +144,8 @@ function Tweet({ tweet }: Props) {
           ))}
         </div>
       )}
-        </div>
-    )
+    </div>
+  );
 }
 
-export default Tweet
+export default Tweet;
